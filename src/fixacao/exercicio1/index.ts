df9fe16d23681150aaf0a7bad3e71ad563ffbbad
bruhoffman <bruhoffman@gmail.com>
2024-07-02 17:15:06 -0300

    Exercícios de sala de aula

--- src/fixacao/exercicio1/index.ts
+++ src/fixacao/exercicio1/index.ts
@@ -9,3 +9,40 @@ que todos os objetos tenham as mesmas propriedades.
 
 3. Modifique o type Pessoa para que possamos escolher apenas entre as cores do arco-íris. Utilize um enum para isso.
 */
+
+enum COR {
+  VERMELHO = "vermelho",
+  ROSA = "rosa",
+  AMARELO = "amarelo",
+  VERDE = "verde",
+  AZUL = "azul",
+  VIOLETA = "violeta"
+}
+
+type TPessoa = {
+  nome: string,
+  idade: number | string,
+  corFavorita: COR
+}
+
+const pessoa1: TPessoa = {
+  nome: "Ana",
+  idade: 20,
+  corFavorita: COR.VERMELHO
+}
+
+const pessoa2: TPessoa = {
+  nome: "Beatriz",
+  idade: 22,
+  corFavorita: COR.AMARELO
+}
+
+const pessoa3: TPessoa = {
+  nome: "Carla",
+  idade: 21,
+  corFavorita: COR.ROSA
+}
+
+const pessoas = [pessoa1, pessoa2, pessoa3]
+
+console.table(pessoas)
--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,51 @@ Crie um sistema de cadastro de usuários que contenha:
 3. Crie exemplos de usuários Admin e Normal;
 */
 
+type TPerson = {
+    id: number | string,
+    name: string,
+    email: string,
+    password: string,
+    role: USER_ROLE
+}
 
+enum USER_ROLE {
+    ADMIN = "administrador",
+    NORMAL = "normal"
+}
 
+type TAdminAccount = {
+    account: string,
+    permission: string
+}
 
+type TNormalAccount = {
+    account: string,
+    permission: string
+}
 
+const administrator: TPerson & TAdminAccount = {
+    id: "123",
+    name: "Bruno",
+    email: "bruno@email.com",
+    password: "321654",
+    role: USER_ROLE.ADMIN,
+    account: "administrador",
+    permission: "geral"
+}
 
+const normal: TPerson & TNormalAccount = {
+    id: "456",
+    name: "Juliano",
+    email: "juliano@email.com",
+    password: "987654",
+    role: USER_ROLE.NORMAL,
+    account: "normal",
+    permission: "normal"
+}
 
-
-
-
-
-
-
-
+console.table(administrator)
+console.table(normal)
 
 
 /* PRÁTICA GUIADA - Parte 2
